e8107b0be2a2b233f58ad741f762c162f69fb9ee
Harish Bohara <harish.bohara@gmail.com>
2023-01-03 00:28:18 +0530

    added a class gp3-delete with retain policy delete and setup monitoring also

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
- helm template ./helm/ --name-template=prod -n aerospike -f ./helm/values.yaml -f ./helm/ext/values_ebs.yaml --set dbReplicas=3 > cluster.yaml
+ helm template ./helm/ --name-template=prod -n aerospike -f ./helm/values.yaml -f ./helm/ext/values_ebs.yaml \
+  --set enableAerospikePrometheusExporter=true \
+  --set enableAerospikeMonitoring=true \
+  --set dbReplicas=3 > cluster.yaml
